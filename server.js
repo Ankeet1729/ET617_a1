7012542678f9f343974579264bd1e05076d1fd5e
Ankeet1729 <ankeet1728@gmail.com>
2025-08-06 09:26:51 +0200

    Added code for accepting non tls supported website for testing Render database

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
+    ssl: { rejectUnauthorized: false }
 });
 
 // Test database connection
